1422fb437608e2ee07e7bf09f0d372e946294313
Bluebender <olliviev@gmail.com>
2023-01-05 14:12:19 +0000

    Sauvegarde du lendemain 15h

--- 03-les fonctions/02-bonus/index.js
+++ 03-les fonctions/02-bonus/index.js
@@ -9,39 +9,37 @@
 let numbers = [23,42,21,90,54,362,-12,57,78,84,35,546,-128,7,15,65,28];
 let ordered_numbers = [];
 
-let smaller = numbers[0];
-let bigger = numbers[0];
 
-function sortNumbers(tab){
-
-        for (number of tab){
-            if (number <= smaller){
-                smaller = number;
-                console.log(`smaller ${smaller}`);
-            }
-            if (number >= bigger){
-                bigger = number;
-                console.log(`bigger ${bigger}`);
-            }
+function sortNumbers(numbers){
+    let min = numbers[0];
+    let max = numbers[0];
+    for (number of numbers){
+        if (number < min){
+            min = number;
         }
-        console.log("fin de la boucle");
-        console.log(`smaller ${smaller}`);
-        console.log(`bigger ${bigger}`);
-
+    }
+    for (number of numbers){
+        if (number > max){
+            max = number;
+        }
+    }
+    console.log(min);
+    console.log(max);
+    ordered_numbers.push(min);
+    console.log(ordered_numbers);
+    let mintemp = min;
+    for (i=0; i<numbers.length-1; i++){
+        let variable = max;
         for (number of numbers){
-            let min = bigger;
-            if (number <= min && number > bigger_stocked){
-                min = number;
+            if (number < variable && number > mintemp){
+                variable = number;
             }
         }
-
-
-        ordered_numbers.push(min);
-        smaller_stocked = smaller;
-        console.log(smaller);
-        console.log(smaller_stocked);
-
-}
-
+        console.log(variable)
+        ordered_numbers.push(variable)
+        mintemp=variable
+    }
+} 
 sortNumbers(numbers);
-console.log(ordered_numbers);
+console.log(ordered_numbers)
+
